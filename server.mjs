ca7eeeee299e4cbf507e764a524476453c53fe03
Abril-cantera <abril.gonzalez.cantera@gmail.com>
2023-03-14 15:12:47 -0300

    Conectando con servidor

--- server.mjs
+++ server.mjs
@@ -1,8 +1,36 @@
 import express from 'express'
+const cors = require('cors');
+
+const routerApi = require('./routes');
+
+const { logErrors, errorHandler, boomErrorHandler, ormErrorHandler } = require('./middlewares/error.handler');
+
 const app = express()
-app.get('/nuevo', (req, res) => {
-  res.end('Hellow Word')
-})
+const port = process.env.PORT || 3000;
+app.use(express.json());
+
+const whitelist = ['http://18.116.45.121:3000', 'http://18.116.45.121:3000/winners']
+
+const options = {
+  origin: (origin, callback) => {
+    if (whitelist.includes(origin) || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error('no permitido'));
+    }
+  }
+}
+app.use(cors(options));
+routerApi(app);
+
+app.use(logErrors);
+app.use(ormErrorHandler);
+app.use(boomErrorHandler);
+app.use(errorHandler);
+
+// app.get('/nuevo', (req, res) => {
+//   res.end('Hellow Word')
+// })
 
 app.listen(3000)
 console.log('====================================');
