a3886a61cdac1ef9bf1c6e4278abb98bdea57af7
Abril-cantera <abril.gonzalez.cantera@gmail.com>
2023-03-14 17:30:18 -0300

    Conectando con servidor

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const app = express()
 const port = process.env.PORT || 3000;
 app.use(express.json());
 
-const whitelist = ['http://18.116.45.121:3000', 'http://18.116.45.121:3000/winners']
+const whitelist = ['http://18.116.45.121:3000', 'http://18.116.45.121:3000/winners', 'http://localhost:5500']
 
 const options = {
   origin: (origin, callback) => {
